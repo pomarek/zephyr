fa679461fdb37e751f2afd1a679df4ee755c46da
Marek Porwisz <marek.porwisz@gmail.com>
2020-04-08 21:22:44 +0200

    output compare reworked

--- drivers/ocmp/oc_stm32.c
+++ drivers/ocmp/oc_stm32.c
@@ -24,10 +24,12 @@ LOG_MODULE_REGISTER(output_cmp, CONFIG_OUTPUT_CMP_LOG_LEVEL);
 
 #define OC_UNITS 4
 
+
 struct oc_stm32_config {
 	TIM_TypeDef *tim;
 	u16_t prescaler;
 	struct stm32_pclken pclken;
+	void (*timer_stm32_irq_config)(void);
 };
 
 struct oc_stm32_data {
@@ -43,73 +45,7 @@ struct oc_stm32_data {
 	((const struct oc_stm32_config * const)(dev)->config->config_info)
 #define TIM_STRUCT(dev) ((TIM_TypeDef *)(DEV_CFG(dev))->tim)
 
-// static u32_t __get_tim_clk(u32_t bus_clk, clock_control_subsys_t *sub_system)
-// {
-// 	struct stm32_pclken *pclken = (struct stm32_pclken *)(sub_system);
-// 	u32_t tim_clk;
-// 	u32_t apb_psc = 0;
 
-// #if defined(CONFIG_SOC_SERIES_STM32H7X)
-// 	if (pclken->bus == STM32_CLOCK_BUS_APB1) {
-// 		apb_psc = CONFIG_CLOCK_STM32_D2PPRE1;
-// 	} else {
-// 		apb_psc = CONFIG_CLOCK_STM32_D2PPRE2;
-// 	}
-
-// 	/*
-// 	 * Depending on pre-scaler selection (TIMPRE), timer clock frequency
-// 	 * is defined as follows:
-// 	 *
-// 	 * - TIMPRE=0: If the APB prescaler (PPRE1, PPRE2) is configured to a
-// 	 *   division factor of 1 then the timer clock equals to APB bus clock.
-// 	 *   Otherwise the timer clock is set to twice the frequency of APB bus
-// 	 *   clock.
-// 	 * - TIMPRE=1: If the APB prescaler (PPRE1, PPRE2) is configured to a
-// 	 *   division factor of 1, 2 or 4, then the timer clock equals to HCLK.
-// 	 *   Otherwise, the timer clock frequencies are set to four times to
-// 	 *   the frequency of the APB domain.
-// 	 */
-// 	if (LL_RCC_GetTIMPrescaler() == LL_RCC_TIM_PRESCALER_TWICE) {
-// 		if (apb_psc == 1U) {
-// 			tim_clk = bus_clk;
-// 		} else {
-// 			tim_clk = bus_clk * 2U;
-// 		}
-// 	} else {
-// 		if (apb_psc == 1U || apb_psc == 2U || apb_psc == 4U) {
-// 			tim_clk = SystemCoreClock;
-// 		} else {
-// 			tim_clk = bus_clk * 4U;
-// 		}
-// 	}
-// #else
-// 	if (pclken->bus == STM32_CLOCK_BUS_APB1) {
-// 		apb_psc = CONFIG_CLOCK_STM32_APB1_PRESCALER;
-// 	}
-// #if !defined(CONFIG_SOC_SERIES_STM32F0X) && !defined(CONFIG_SOC_SERIES_STM32G0X)
-// 	else {
-// 		apb_psc = CONFIG_CLOCK_STM32_APB2_PRESCALER;
-// 	}
-// #endif
-
-// 	/*
-// 	 * If the APB prescaler equals 1, the timer clock frequencies
-// 	 * are set to the same frequency as that of the APB domain.
-// 	 * Otherwise, they are set to twice (×2) the frequency of the
-// 	 * APB domain.
-// 	 */
-// 	if (apb_psc == 1U) {
-// 		tim_clk = bus_clk;
-// 	} else	{
-// 		tim_clk = bus_clk * 2U;
-// 	}
-// #endif
-
-// 	return tim_clk;
-// }
-
-
-//static void timer_stm32_irq_config(struct device *dev);
 
 static int oc_stm32_start(struct device *dev,
 			  const struct counter_cfg * ovf_cfg)
@@ -126,24 +62,12 @@ static int oc_stm32_start(struct device *dev,
 	}
 
 #ifdef CONFIG_SOC_SERIES_STM32F1X
-	/* FIXME: IS_TIM_32B_COUNTER_INSTANCE not available on
-	 * SMT32F1 Cube HAL since all timer counters are 16 bits
-	 */
 	counter_32b = 0;
 #else
 	counter_32b = IS_TIM_32B_COUNTER_INSTANCE(TIM_STRUCT(dev));
 #endif
 
-	/*
-	 * The timer counts from 0 up to the value in the ARR register (16-bit).
-	 * Thus period_cycles cannot be greater than UINT16_MAX + 1.
-	 */
 	if (!counter_32b && (ovf_cfg->counter_val > 0x10000)) {
-		/* 16 bits counter does not support requested period
-		 * You might want to adapt PWM output clock to adjust
-		 * cycle durations to fit requested period into 16 bits
-		 * counter
-		 */
 		return -ENOTSUP;
 	}
 
@@ -157,12 +81,16 @@ static int oc_stm32_start(struct device *dev,
 	/* Set period value */
 	data->tim_handle.Init.Period = ovf_cfg->counter_val - 1;
 
-	if (HAL_TIM_Base_Init(&data->tim_handle) != HAL_OK) {
+	if (HAL_TIM_OC_Init(&data->tim_handle) != HAL_OK) {
 		return -EACCES;
 	}
+
 	if (HAL_TIM_Base_Start_IT(&data->tim_handle) != HAL_OK) {
 		return -EACCES;
 	}
+
+	data->timer_cfg.ovf_match_callback = ovf_cfg->ovf_match_callback;
+	data->timer_cfg.user_data = ovf_cfg->user_data;
 
 	return 0;
 }
@@ -174,16 +102,102 @@ static int oc_stm32_stop(struct device *dev)
 	}
 	return 0;
 }
+static s32_t num_to_channel(u8_t chan_num)
+{
+	switch(chan_num) {
+	case 0:
+		return TIM_CHANNEL_1;
+	case 1:
+		return TIM_CHANNEL_2;
+	case 2:
+		return TIM_CHANNEL_3;
+	case 3:
+		return TIM_CHANNEL_4;
+	default:
+		return -1;
+	}
+}
 
 static int oc_stm32_set_compare(struct device *dev, u8_t channel,
 				const struct counter_cfg *match_cfg)
 {
+	struct oc_stm32_data *data = DEV_DATA(dev);
+
+	if (channel >= OC_UNITS || match_cfg == NULL) {
+		return -EINVAL;
+	}
+	if(match_cfg->ovf_match_callback == NULL) {
+		HAL_TIM_OC_Stop_IT(&data->tim_handle, num_to_channel(channel));
+		data->compare_unit[channel].ovf_match_callback = NULL;
+		return 0;
+	}
+
+	if (match_cfg->counter_val > data->tim_handle.Init.Period) {
+		return -EINVAL;
+	}
+
+	if (!IS_TIM_INSTANCE(TIM_STRUCT(dev))) {
+		return -ENOTSUP;
+	}
+
+	switch (channel) {
+	case 0:
+		LL_TIM_OC_EnablePreload(TIM_STRUCT(dev), LL_TIM_CHANNEL_CH1);
+		TIM_STRUCT(dev)->CCR1 = match_cfg->counter_val;
+		break;
+	case 1:
+		LL_TIM_OC_EnablePreload(TIM_STRUCT(dev), LL_TIM_CHANNEL_CH2);
+		TIM_STRUCT(dev)->CCR2 = match_cfg->counter_val;
+		break;
+	case 2:
+		LL_TIM_OC_EnablePreload(TIM_STRUCT(dev), LL_TIM_CHANNEL_CH3);
+		TIM_STRUCT(dev)->CCR3 = match_cfg->counter_val;
+		break;
+	case 3:
+		LL_TIM_OC_EnablePreload(TIM_STRUCT(dev), LL_TIM_CHANNEL_CH4);
+		TIM_STRUCT(dev)->CCR4 = match_cfg->counter_val;
+		break;
+	default:
+		return -EINVAL;
+	}
+
+	data->compare_unit[channel].ovf_match_callback = match_cfg->ovf_match_callback;
+	data->compare_unit[channel].user_data = match_cfg->user_data;
+
+
+	if (HAL_TIM_OC_Start_IT(&data->tim_handle, num_to_channel(channel)) != HAL_OK) {
+		return -EACCES;
+	}
+
 	return 0;
 }
 
 static int oc_stm32_update_compare(struct device *dev, u8_t channel,
 				   u32_t match)
 {
+	struct oc_stm32_data *data = DEV_DATA(dev);
+
+	if (match > data->tim_handle.Init.Period) {
+		return -EINVAL;
+	}
+
+	switch (channel) {
+	case 0:
+		TIM_STRUCT(dev)->CCR1 = match;
+		break;
+	case 1:
+		TIM_STRUCT(dev)->CCR2 = match;
+		break;
+	case 2:
+		TIM_STRUCT(dev)->CCR3 = match;
+		break;
+	case 3:
+		TIM_STRUCT(dev)->CCR4 = match;
+		break;
+	default:
+		return -EINVAL;
+	}
+
 	return 0;
 }
 
@@ -191,25 +205,52 @@ static u32_t oc_stm32_get_counter(struct device *dev)
 {
 	return TIM_STRUCT(dev)->CNT;
 }
-// static int rtc_stm32_set_alarm(struct device *dev,
-// 				const struct counter_alarm_cfg *alarm_cfg)
-// {
-// 	return 0;
-// }
-// HAL_StatusTypeDef HAL_TIM_OC_Init(TIM_HandleTypeDef *htim);
-// HAL_StatusTypeDef HAL_TIM_OC_DeInit(TIM_HandleTypeDef *htim);
-// void HAL_TIM_OC_MspInit(TIM_HandleTypeDef *htim);
-// void HAL_TIM_OC_MspDeInit(TIM_HandleTypeDef *htim);
-// /* Blocking mode: Polling */
-// HAL_StatusTypeDef HAL_TIM_OC_Start(TIM_HandleTypeDef *htim, uint32_t Channel);
-// HAL_StatusTypeDef HAL_TIM_OC_Stop(TIM_HandleTypeDef *htim, uint32_t Channel);
-// /* Non-Blocking mode: Interrupt */
-// HAL_StatusTypeDef HAL_TIM_OC_Start_IT(TIM_HandleTypeDef *htim, uint32_t Channel);
-// HAL_StatusTypeDef HAL_TIM_OC_Stop_IT(TIM_HandleTypeDef *htim, uint32_t Channel);
 
 void oc_stm32_isr(void *arg)
 {
+	struct device *dev = (struct device *)arg;
+	struct oc_stm32_data *data = DEV_DATA(dev);
+	const struct oc_stm32_config *cfg = DEV_CFG(dev);
 
+	if (cfg->tim->SR & TIM_IT_UPDATE) {
+		cfg->tim->SR = ~TIM_IT_UPDATE;
+		if (data->timer_cfg.ovf_match_callback) {
+			data->timer_cfg.ovf_match_callback(dev,
+							data->tim_handle.Init.Period+1,
+							data->timer_cfg.user_data);
+		}
+	}
+
+	if (cfg->tim->SR & TIM_IT_CC1) {
+		cfg->tim->SR = ~TIM_IT_CC1;
+		if (data->compare_unit[0].ovf_match_callback) {
+			data->compare_unit[0].ovf_match_callback(dev,
+				data->tim_handle.Instance->CCR1,
+				data->compare_unit[0].user_data);
+		}
+	}
+	if (cfg->tim->SR & TIM_IT_CC2) {
+		cfg->tim->SR = ~TIM_IT_CC2;
+		if (data->compare_unit[1].ovf_match_callback) {
+			data->compare_unit[1].ovf_match_callback(dev,
+				data->tim_handle.Instance->CCR2,
+				data->compare_unit[1].user_data);
+		}
+	}if (cfg->tim->SR & TIM_IT_CC3) {
+		cfg->tim->SR = ~TIM_IT_CC3;
+		if (data->compare_unit[2].ovf_match_callback) {
+			data->compare_unit[2].ovf_match_callback(dev,
+				data->tim_handle.Instance->CCR3,
+				data->compare_unit[2].user_data);
+		}
+	}if (cfg->tim->SR & TIM_IT_CC4) {
+		cfg->tim->SR = ~TIM_IT_CC4;
+		if (data->compare_unit[3].ovf_match_callback) {
+			data->compare_unit[3].ovf_match_callback(dev,
+				data->tim_handle.Instance->CCR4,
+				data->compare_unit[3].user_data);
+		}
+	}
 }
 
 static inline void __oc_stm32_get_clock(struct device *dev)
@@ -235,6 +276,11 @@ static int oc_stm32_init(struct device *dev)
 		return -EIO;
 	}
 
+	if(config->timer_stm32_irq_config) {
+		config->timer_stm32_irq_config();
+	}
+
+
 	return 0;
 }
 
@@ -246,61 +292,98 @@ static const struct output_cmp_driver_api api = {
 	.get_counter = oc_stm32_get_counter,
 };
 
+#define CONNECT_IRQ(inst, id)						\
+do {									\
+	IRQ_CONNECT(DT_INST_IRQ_BY_IDX(inst, id, irq), 			\
+		    DT_INST_IRQ_BY_IDX(inst, id, priority), 		\
+		    oc_stm32_isr, 					\
+		    &__device_oc_stm32_##inst,				\
+		    0);							\
+	irq_enable(DT_INST_IRQ_BY_IDX(inst, id, irq));	    		\
+} while(false)
 
-// static void stm32_prec_irq_config(struct device *dev)
-// {
-// 	//IRQ_CONNECT(DT_INST_IRQN(0),
-// 	//	    DT_INST_IRQ(0, priority),
-// 	//	    rtc_stm32_isr, DEVICE_GET(rtc_stm32), 0);
-// 	//irq_enable(DT_INST_IRQN(0));
-// }
+
+#define IRQ_CFG_FUN1(inst)						\
+static void timer_stm32_irq_config_##inst(void)				\
+{									\
+	CONNECT_IRQ(inst, 0);						\
+}
+
+#define IRQ_CFG_FUN2(inst)						\
+static void timer_stm32_irq_config_##inst(void)				\
+{									\
+	CONNECT_IRQ(inst, 0);						\
+	CONNECT_IRQ(inst, 1);						\
+}
 
 
 #define CREATE_OC_DEV(inst)						\
+									\
+static void timer_stm32_irq_config_##inst(void);			\
+									\
 static struct oc_stm32_data timer_data_##inst = {			\
 };									\
 									\
 static const struct oc_stm32_config timer_config_##inst = {		\
-	.tim = (TIM_TypeDef*)DT_INST_##inst##_ST_STM32_TIMERS_BASE_ADDRESS, \
+	.tim = (TIM_TypeDef*)DT_REG_ADDR(DT_INST_PHANDLE(inst, timer)), \
 	.pclken = {							\
-		.bus = DT_INST_##inst##_ST_STM32_TIMERS_CLOCK_BUS,	\
-		.enr = DT_INST_##inst##_ST_STM32_TIMERS_CLOCK_BITS	\
+		.bus = DT_CLOCKS_CELL(DT_INST_PHANDLE(inst, timer), bus), \
+		.enr = DT_CLOCKS_CELL(DT_INST_PHANDLE(inst, timer), bits), \
 	},								\
+	.prescaler = DT_INST_PROP(inst, st_prescaler),			\
+	.timer_stm32_irq_config = timer_stm32_irq_config_##inst, 	\
 };									\
 									\
-DEVICE_AND_API_INIT(prec_timer_##inst, DT_INST_LABEL(inst), 		\
+DEVICE_AND_API_INIT(oc_stm32_##inst, DT_INST_LABEL(inst), 		\
 		    &oc_stm32_init, &timer_data_##inst, 		\
 		    &timer_config_##inst, PRE_KERNEL_1, 		\
-		    CONFIG_KERNEL_INIT_PRIORITY_DEVICE, &api);
+		    CONFIG_KERNEL_INIT_PRIORITY_DEVICE, &api);		\
+
+
 
 #if DT_HAS_NODE(DT_DRV_INST(0))
 CREATE_OC_DEV(0);
+#if DT_INST_IRQ_HAS_IDX(0, 1)
+IRQ_CFG_FUN2(0)
+#else
+IRQ_CFG_FUN1(0)
 #endif
+#endif
+
 
 #if DT_HAS_NODE(DT_DRV_INST(1))
 CREATE_OC_DEV(1);
+#if DT_INST_IRQ_HAS_IDX(1, 1)
+IRQ_CFG_FUN2(1)
+#else
+IRQ_CFG_FUN1(1)
+#endif
 #endif
 
 #if DT_HAS_NODE(DT_DRV_INST(2))
 CREATE_OC_DEV(2);
+#if DT_INST_IRQ_HAS_IDX(2, 1)
+IRQ_CFG_FUN2(2)
+#else
+IRQ_CFG_FUN1(2)
+#endif
 #endif
 
 #if DT_HAS_NODE(DT_DRV_INST(3))
 CREATE_OC_DEV(3);
+#if DT_INST_IRQ_HAS_IDX(3, 1)
+IRQ_CFG_FUN2(3)
+#else
+IRQ_CFG_FUN1(3)
+#endif
 #endif
 
 #if DT_HAS_NODE(DT_DRV_INST(4))
 CREATE_OC_DEV(4);
+#if DT_INST_IRQ_HAS_IDX(4, 1)
+IRQ_CFG_FUN2(4)
+#else
+IRQ_CFG_FUN1(4)
+#endif
 #endif
 
-#if DT_HAS_NODE(DT_DRV_INST(5))
-CREATE_OC_DEV(5);
-#endif
-
-#if DT_HAS_NODE(DT_DRV_INST(6))
-CREATE_OC_DEV(6);
-#endif
-
-#if DT_HAS_NODE(DT_DRV_INST(7))
-CREATE_OC_DEV(7);
-#endif
